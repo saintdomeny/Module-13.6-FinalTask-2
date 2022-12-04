e0bcd566c1c75e8ecaa2266db5759c2e09e5455a
Nikula Daniil <daniil.nikula@gmail.com>
2022-12-04 15:25:16 +0200

    Добавьте файлы проекта.

---
+++ Module 13.6 FinalTask 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Diagnostics.Tracing;
+
+namespace Module_13._6_FinalTask_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("D:\\Domeny\\Downloads\\Text1.txt");
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+
+            var words = noPunctuationText.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            var repeatedWords = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                bool cont = repeatedWords.ContainsKey(word);
+                int count;
+                if (cont)
+                {
+                    repeatedWords.TryGetValue(word, out count);
+                    repeatedWords.Remove(word);
+                    repeatedWords.Add(word, count+1);
+                }
+                else
+                {
+                    repeatedWords.Add(word, 1);
+                }
+            }
+            //Output of first 10 words.
+            int i = 0;
+            foreach (KeyValuePair<string, int> word in repeatedWords.OrderByDescending(key => key.Value))
+            {
+                if (i < 10)
+                {
+                    Console.WriteLine("Word: {0}, Repeats: {1}", word.Key, word.Value);
+                    i++;
+                }
+            }
+
+        }
+    }
+}
